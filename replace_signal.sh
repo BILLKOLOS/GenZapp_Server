a321e49242203e56556fcd2dd51bf761403c0b58
billkolos <billooko2@gmail.com>
2024-08-15 14:16:07 +0300

    signal changed to Africa Territory

---
+++ replace_signal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Directory to search
+DIR="/data/data/com.termux/files/home/GenZapp/app"
+
+# Find and replace all instances of 'GenZapp' with 'GenZapp'
+find $DIR -type f -exec sed -i 's/GenZapp/GenZapp/g' {} +
+find $DIR -type f -exec sed -i 's/GenZapp/GenZapp/g' {} +
+find $DIR -type f -exec sed -i 's/GenZapp/GenZapp/g' {} +
+find $DIR -type f -exec sed -i 's/[Ss][Ii][Gg][Nn][Aa][Ll]/GenZapp/g' {} +
+
+echo "Replacement complete."
